eb364bdf7cbb23eab3418d2218eac0615b449083
Stephen Silber <stephensilber@gmail.com>
2023-04-25 20:06:16 -0400

    Update StackNavigationViewController.swift

--- Sources/ComposableNavigation/StackNavigation/StackNavigationViewController.swift
+++ Sources/ComposableNavigation/StackNavigation/StackNavigationViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 import ComposableArchitecture
 
 /// A convenience UINavigationController implementation containing a `StackNavigationHandler`.
-public class StackNavigationViewController<ViewProvider: ViewProviding>: UINavigationController {
+open class StackNavigationViewController<ViewProvider: ViewProviding>: UINavigationController {
 	internal let navigationHandler: StackNavigationHandler<ViewProvider>
 	
-	public convenience init(
+	open convenience init(
 		store: Store<StackNavigation<ViewProvider.Item>.State, StackNavigation<ViewProvider.Item>.Action>,
 		viewProvider: ViewProvider
 	) {
 		self.init(navigationHandler: StackNavigationHandler(store: store, viewProvider: viewProvider))
 	}
 	
-	public init(navigationHandler: StackNavigationHandler<ViewProvider>) {
+	open init(navigationHandler: StackNavigationHandler<ViewProvider>) {
 		self.navigationHandler = navigationHandler
 		super.init(nibName: nil, bundle: nil)
 		self.navigationHandler.setup(with: self)
 	}
 	
-	public required init?(coder: NSCoder) {
+	open required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
 }
